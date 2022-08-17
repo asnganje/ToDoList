cde440c5998556331ca838b8368c9770331b1a94
asnganje <abd.nganje@gmail.com>
2022-08-17 22:02:38 +0300

    add index html, js files and style.css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import * as todoList from './todolist.js';
+import * as addRemove from './add-remove.js';
+import * as storage from './storage.js';
+
+const tasks = storage.getTasksFromStorage();
+
+todoList.TodoListItems(tasks);
+addRemove.TodoListAdd(tasks);
+addRemove.TodoListEdit(tasks);
+addRemove.TodoListDelete(tasks);
